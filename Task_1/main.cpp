1fcf1f1ee4767d4fe3c4526709cbd8103c72f92e
LuminousAurora <to_be_a_racoon@mail.ru>
2021-06-14 13:59:50 +0300

    Week_1

---
+++ Task_1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout
+        << "      *      " << std::endl
+        << "     * *     " << std::endl
+        << "    *   *    " << std::endl
+        << "   *******   " << std::endl
+        << "  *       *  " << std::endl
+        << " *         * " << std::endl;
+
+    std::getchar();
+
+
+    return 0;
+
+}
